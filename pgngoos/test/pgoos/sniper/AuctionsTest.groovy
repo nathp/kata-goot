f6271de78dfe5133f9e3eb1006579aafc1a52cd6
prasanth <lavanya@.(none)>
2011-05-27 21:57:27 +0530

    Renamed test

--- pgngoos/test/pgoos/sniper/AuctionsTest.groovy
+++ pgngoos/test/pgoos/sniper/AuctionsTest.groovy
@@ -3,7 +3,7 @@ import org.junit.*
 import static org.mockito.Mockito.*
 import junit.framework.TestCase
 
-public class AuctionsTestTest extends TestCase {
+public class AuctionsTest extends TestCase {
 
     void test_creates_fresh_Auction() {
         assertNotNull new Auctions().autionFor("unknown")
