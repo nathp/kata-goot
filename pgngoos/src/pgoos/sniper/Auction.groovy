108413736fd5492c71795f6d661ffb1b1507bf3f
prasanth <lavanya@.(none)>
2011-06-06 14:01:51 +0530

    Removed state from Auction

--- pgngoos/src/pgoos/sniper/Auction.groovy
+++ pgngoos/src/pgoos/sniper/Auction.groovy
@@ -12,55 +12,6 @@ import pgoos.sniper.events.*
  */
 class Auction {
 
-
-    private interface State {
-        void handle(AuctionEvent msg)
-
-        boolean isLoosing(Bid bid)
-    }
-
-    class InitialState implements State {
-        @Override
-        void handle(AuctionEvent msg) {
-            msg.handle(listener, Auction.this)
-            switchTo(HANDLE_BID)
-        }
-
-        @Override
-        boolean isLoosing(Bid bid) {
-            return false
-        }
-
-    }
-    State INITIAL = new InitialState()
-
-    class BidState implements State {
-        @Override
-        void handle(AuctionEvent msg) {
-            msg.handle(listener, Auction.this)
-            postHandle(msg)
-        }
-
-        private def postHandle(AuctionEvent msg) {
-            if (isResponseUpdateForOurOwnBid(msg)) {
-                ourLastBidPrice = msg.price
-            }
-        }
-
-        boolean isResponseUpdateForOurOwnBid(AuctionEvent event) {
-            event instanceof Bid && event.client == clientId
-        }
-
-        boolean isLoosing(Bid bid) {
-            isNotOurBid(bid) && !firstBidUpdate() && bid.isMoreThan(ourLastBidPrice as int)
-        }
-
-        private boolean isNotOurBid(Bid bid) {
-            return !bid.belongsToClient(auctionId)
-        }
-
-    }
-
     boolean firstBidUpdate() {
         ourLastBidPrice == null
     }
@@ -69,71 +20,49 @@ class Auction {
 
     AutoBid autobid
 
-    State HANDLE_BID = new BidState()
-
-    class Closed implements State {
-        @Override
-        void handle(AuctionEvent msg) { }
-
-        @Override
-        boolean isLoosing(Bid bid) {return false }
-    }
-
-    State CLOSED = new Closed()
-
-    class AutoBidState implements State {
-        Sniper sniper
-
-
-        AutoBidState(Sniper sniper) {
-            this.sniper = sniper
-        }
-
-        @Override
-        void handle(AuctionEvent msg) {
-            HANDLE_BID.handle msg
-            if (msg instanceof Bid) {
-                Bid bid = msg
-                autobid.bidHigher bid.price as int, sniper
-            }
-        }
-
-        @Override
-        boolean isLoosing(Bid bid) {
-            HANDLE_BID.isLoosing bid
-        }
-
-    }
-
     String auctionId
     String clientId
     AuctionStateListener listener = AuctionStateListener.NONE
-    State currentState = INITIAL
-
+    Sniper sniper
 
     public String toString() {
         auctionId
     }
 
-    boolean isLoosing(Bid bid) {
-        currentState.isLoosing bid
-    }
-
-    private def switchTo(State state) {
-        currentState = state
-    }
-
     def autoBid(AutoBid autoBid, Sniper sniper) {
         this.autobid = autoBid
-        switchTo new AutoBidState(sniper)
+        this.sniper = sniper
     }
 
     def update(AuctionMessage message) {
-        currentState.handle(EventFactory.createFrom(message))
+        def event = EventFactory.createFrom(message)
+        event.handle(listener, this)
+        if (event instanceof Bid) {
+            Bid bid = event
+            autobid?.bidHigher bid.price as int, sniper
+        }
+        postHandle(event)
+    }
+
+    private def postHandle(AuctionEvent msg) {
+        if (isResponseUpdateForOurOwnBid(msg)) {
+            ourLastBidPrice = msg.price
+        }
+    }
+
+    boolean isResponseUpdateForOurOwnBid(AuctionEvent event) {
+        event instanceof Bid && event.client == clientId
+    }
+
+    boolean isLoosing(Bid bid) {
+        isNotOurBid(bid) && !firstBidUpdate() && bid.isMoreThan(ourLastBidPrice as int)
+    }
+
+    private boolean isNotOurBid(Bid bid) {
+        return !bid.belongsToClient(auctionId)
     }
 
     void close() {
-        switchTo CLOSED
     }
 
     boolean exceededStopPrice(Bid bid) {
